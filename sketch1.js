7bbb79f4310d7bd73f0f7ed47388c1c10cb03cd0
Jacob Cassell <jacobschoolemail@gmail.com>
2018-04-05 14:28:48 -0700

    fuckin work

--- sketch1.js
+++ sketch1.js
@@ -1,5 +1,16 @@
 function setup() {
   createCanvas(500, 500);
+  fill(19,168,0)
+  beginShape();
+  vertex(100,100);
+  vertex(150,50);
+  vertex(150,75);
+  vertex(200,50);
+  vertex(200,75);
+  vertex(250,50);
+  vertex(250,100);
+  vertex(100,100);
+  endShape();
 }
 
 function draw() {
