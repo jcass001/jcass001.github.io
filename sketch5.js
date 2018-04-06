de808820cfa4efa95701d19f795a255cb955f583
Jacob Cassell <jacobschoolemail@gmail.com>
2018-04-05 22:05:08 -0700

    now it's done

--- sketch5.js
+++ sketch5.js
@@ -1,16 +1,5 @@
 function setup() {
   createCanvas(500, 500);
-  fill(19,168,0)
-  beginShape();
-  vertex(200,300);
-  vertex(250,250);
-  vertex(250,275);
-  vertex(300,250);
-  vertex(300,275);
-  vertex(350,250);
-  vertex(350,300);
-  vertex(200,300);
-  endShape();
 }
 
 function draw() {
--- sketch6.js
+++ sketch6.js
@@ -1,6 +1,17 @@
 function setup() {
   createCanvas(500, 500);
   background(83, 84, 86)
+  fill(19,168,0)
+  beginShape();
+  vertex(200,300);
+  vertex(250,250);
+  vertex(250,275);
+  vertex(300,250);
+  vertex(300,275);
+  vertex(350,250);
+  vertex(350,300);
+  vertex(200,300);
+  endShape();
 }
 
 function draw() {
